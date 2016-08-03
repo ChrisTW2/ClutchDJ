8f415137edc3b53e00dc3cf715b9a4a3d2e5f001
dsuappacademy <worsleychris10@gmail.com>
2016-08-03 14:41:47 -0400

    Initial commit

---
+++ app/src/main/java/edu/desu/dsuappacademy/clutchdj/Splash.java
@@ -0,0 +1,40 @@
+package edu.desu.dsuappacademy.clutchdj;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by dsuappacademy on 8/2/16.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(6000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
